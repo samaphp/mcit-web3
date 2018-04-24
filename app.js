57cc71cbac6b1892529f4e423d20837300b9ca33
samaphp <samaphp@gmail.com>
2018-04-24 12:47:08 +0300

    Uncomment getAccounts and getTransaction.

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ web3.eth.getAccounts(function(err, accounts){
 web3.eth.getBalance('0x5300670e39956cc2682634e925b98b426ba18a74', function(err, balance){
 	console.log(err ? err : balance);
 });
-/*
 
 web3.eth.getAccounts(function(err, accounts){
 	var account1 = accounts[1];
@@ -24,8 +23,6 @@ web3.eth.getAccounts(function(err, accounts){
 	});
 });
 
-
 web3.eth.getTransaction('0x7862d682229071baf680fc6ec6719a70ed97f5a28eda114b0775ada1a63b9b21', function(err, tx){
 	console.log(err ? err : tx);
 });
-*/
