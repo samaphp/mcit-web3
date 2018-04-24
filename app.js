1fab83252b52ae7673ec08e7a493c93473eec1e4
samaphp <samaphp@gmail.com>
2018-04-24 12:48:57 +0300

    Adding some comments to explain the code.

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const ganache = new web3.providers.HttpProvider('http://localhost:8545');
 
 web3.setProvider(ganache);
 
+// Getting all local accounts.
 web3.eth.getAccounts(function(err, accounts){
 	console.log(err ? err : accounts);
 });
 
-
+// Getting the balance of my first account.
 web3.eth.getBalance('0x5300670e39956cc2682634e925b98b426ba18a74', function(err, balance){
 	console.log(err ? err : balance);
 });
 
+// Getting my account transactions.
 web3.eth.getAccounts(function(err, accounts){
 	var account1 = accounts[1];
 	var account2 = accounts[2];
@@ -23,6 +25,7 @@ web3.eth.getAccounts(function(err, accounts){
 	});
 });
 
+// Showing transaction details.
 web3.eth.getTransaction('0x7862d682229071baf680fc6ec6719a70ed97f5a28eda114b0775ada1a63b9b21', function(err, tx){
 	console.log(err ? err : tx);
 });
